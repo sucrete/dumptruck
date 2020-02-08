4ae3b92a0e8cffbb7b99941c57b35705940077aa
William Andree <williamandree@gmail.com>
2020-02-08 11:09:33 -0600

    adding dumps to website

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9859649a83d129655517";
+/******/ 	var hotCurrentHash = "e93ab908a27df4a0db34";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
