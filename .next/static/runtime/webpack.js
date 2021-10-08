9e8b13a54ae7f5234fe2058a11f753e35b678f1b
William Andree <williamandree@gmail.com>
2021-10-08 03:04:25 -0500

    moving image hosting to root

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "58d20bfc1292087500f6";
+/******/ 	var hotCurrentHash = "294faba1190a74d34b41";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
