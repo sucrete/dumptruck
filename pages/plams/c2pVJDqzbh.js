19f7dd60d12174d8edd436d277ce93dbe6fb04e7
William Andree <williamandree@gmail.com>
2021-10-11 16:02:36 -0500

    migrating images

--- pages/plams/c2pVJDqzbh.js
+++ pages/plams/c2pVJDqzbh.js
@@ -1,14 +1,11 @@
 import Dump from "../../components/Dump";
-const sourceURLS = [
-  "https://lh3.googleusercontent.com/_c7EnJeAbbs11joFFPX0GYBGtzH_Y1SMMbnTryDwGHtVicJOIIMycs9-MvqfN2UgiCRghkuiMty9FNelDs17JH6foYpdFvzNGgIM7CTUYC03BegbfBFA8kaGIl7qKdQI1h1Jd8HJCN_-w8QOfIHb68M_mE8UdDahVr9pA5-saA46dP_WRHik--1KoTDMwdH1HmfsyE_K2SHZOyX8pyCNd4oR8hX5zQ3atuE0b1jUoHya7mCJTpP_kji6GRh_DaOQtNLFrt9-5LvOXahYKVemOFmKLrOrImMosTXShuXfwmUQ8qO8nd9Ktstrr1aDIY129t_XXDj88WGRdI71VJVHU4ReaTeqDkO7zR4_zTuNo5Ta2UXsXalayBvF4M6yrmYpPSXSlUqwTWTovKPBMmERlQLRIhwBCwbDF9bDre06_fVVMtnETZs07TvTl_GVstFfKx1FgsXozPOAx82dlZhQxuWrK5jAbp_54JhgAmiVVB7y0rgzodpTmTGvM-5Bva7gu9QY3jwHnFzEJ32CxZNpi0zJapacnVwgeRB6V7PaJeEO2vUwDJLvCeW7ZmrrflLTrswXJjVv5TdyfQjJ7lLcsD0rQ5OF4kxHFMOt6tQAw8bVrVkqpVaNhki8qEIOu5N_hkqg0KCKdy63E2fyPqhPm_Kdxbp8cnx_gm0OAvZDOfwxr8C01K0=w1440-h550-ft",
-  "https://lh3.googleusercontent.com/x9h4QLfF0mcwPm2s0Bj6ZukYQoyob8khz95QulWyi6uruj_Ta-TZ1dk_z73bpO1N-fs0h7_Fcq7r1lC9XQNo_kWM4VhujaAcKqKPUwuCRVMQi1hSQ2TWLF7bUELRij1vUOHlkXZgtWGcQ1jsbyL-gozh0SHzeg7nNct--J5GryEeP4xkdqGMytJ-ATjrUAotv7h70qv4Uh9XPHSA-qQBqoKrtZMe3i3yFaO3oL4qHeKrm8mkoZ7aBdpptCWa-Sp4NH8jnxDRl9SFpwtd7inPpkoGEKJb40zb3CEBRKhUU1fOuhamJdxNHuP8YP1j5c3blV6ePTEIaY3jsr9mpoyhQmMgI7PC_UoBeNzPHTyBh1GcxnTKnBwkgPyblN0KvH5q4nTR_PuG4pzmv5Y4tbFH-dAl0lb1ENlSRR0m050wEyXnvYzKPr_MTQ8J9YvjzT1fjXaqHsZpdEheEndUzym299N6xWsqKVxA6cm1E9hEBT8KnPNm6MLW0tSnE0osoNLyVtpVuAcSoanlJltvsOtLxJ6xtl8W86myPca8-WTWy2fhjpFUFjyUPKE8w1bgeeZkYs5kfA48l-WYwu1Qa7aNgrTswdzAJ6RHbwkDDaTK2JC0XkUy4SAN2PUy3yfFhJ9s2NzOrsuF9YhlU464rVWd_R7zXem0GDkLwQrUXKjQnyEf0_OQ0fI=w1440-h550-ft"
-];
+const sourceURLS = ["/c2pVJDqzbh--0.gif", "/c2pVJDqzbh--1.png"];
 export default function c2pVJDqzbh() {
   return (
     <div>
       <Dump id="plams">
-        {sourceURLS.map(url => (
-          <img className="workComponent" src={url} />
+        {sourceURLS.map((url, index) => (
+          <img className="workComponent" src={url} key={index} />
         ))}
       </Dump>
       <style jsx>{`
--- pages/plams/dY0xNKpMOd.js
+++ pages/plams/dY0xNKpMOd.js
@@ -1,14 +1,10 @@
 import Dump from "../../components/Dump";
-const sourceURLS = [
-  "https://lh3.googleusercontent.com/kaEHV2hVxz1SenBmjrTZNrZUDZwdcUQ4hvxQ0P_8baA_5wP5W_N-5knsrag_N19M2zC_uMVPTc3RGMv-ouFgF5nq4BnpzSmN9gRPrKULTYQArGpabnOwBIfqZZQT2ud8CZRVWv1L0EHet6FPaMOERMQdFVCM8jyoC37K-HLbBywQv5HG6_rADKCT8D1DE1egbbCZ_4dNJtDstiuop-3UQPyJQ8-OJ8SH7tTdrTO_sgWOIWOIc-pnAsnF-i93YduscwvuvHuwNPPliILFGo0fP-vnMmSkYSZQ7K70TBQcSfxq2c77jnv3NKVw9KUy0b1WL3zFRjDGdLenIONnGxaEkK2xmfhWlDsB0kUWr_Zc4n5in4QGXDKJ621-6mbnTDN2T8zd-W6M0PiwMcK32KNNlnPME2cMecciblh4-myZybvk3oZeuKC9rl060BS1_aj6Qk2l9wF3vlUn6VwCTl1GdBhCSY0yG_XedlTHqKf_3gDnQAka_U3eezJ1g4rjTfl4AAIYQlBPIvYQ9UmBXGVxsX6bJxxFqekNm8MXt-y-V6ssJJ493iqB9sZy9ucU94Fwyw5601d8Yh8ox-l9M8zHCFOSfspAb9Dh6K1u4prH-mCzp_k8h7fERb1lb12dZ_4qAZbY4L9R3RhQlP-tdKDBSKrgvdwqnO-oquRTj-6lUhKssVb1P5Q=w1440-h550-ft"
-];
+
 export default function dY0xNKpMOd() {
   return (
     <div>
       <Dump id="plams">
-        {sourceURLS.map(url => (
-          <img className="workComponent" src={url} />
-        ))}
+        <img className="workComponent" src="/dY0xNKpMOd.gif" />
       </Dump>
       <style jsx>{`
         .workComponent {
